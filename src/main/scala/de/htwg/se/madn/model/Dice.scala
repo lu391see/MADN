833d7f2d1c6a6cfa58439ffd402cd7cbcc1da370
lu391see <lukas.seehuber@gmail.com>
2020-05-21 21:05:07 +0200

    reworked DiceSpec and added test for s1 and recipient ++ removed print-method in Dice

--- src/main/scala/de/htwg/se/madn/model/Dice.scala
+++ src/main/scala/de/htwg/se/madn/model/Dice.scala
@@ -4,5 +4,4 @@ import scala.util.Random
 case class Dice(recipient : String) {
   val t1: Int = Random.nextInt(6) + 1
   val s1 = recipient + " has thrown " + t1.toString
-  println(s1)
 }
--- src/test/scala/de/htwg/se/madn/model/DiceSpec.scala
+++ src/test/scala/de/htwg/se/madn/model/DiceSpec.scala
@@ -4,9 +4,16 @@ import org.scalatest.{Matchers, WordSpec}
 class DiceSpec extends WordSpec with Matchers{
   "A Dice " when {
     "thrown" should {
-      val Dice = new Dice("yellow")
+      val dice = new Dice("yellow")
       "have a value between 1 and 6" in {
-        Dice.t1 should (be > 0 and <7)
+        dice.t1.isInstanceOf[Int]
+        dice.t1.toString matches "1|2|3|4|5|6"
+      }
+      "when printed look like" in {
+        dice.s1 matches s"$dice.recipient has thrown $dice.t1"
+      }
+      "recipient as Team" in {
+        dice.recipient should be("yellow")
       }
     }
   }
