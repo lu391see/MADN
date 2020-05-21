acf9fc4b5f9938f2a7fa6a4cf6e19b364cf8b187
lu391see <lukas.seehuber@gmail.com>
2020-05-21 13:50:43 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "MADN"
+
+version := "0.1"
+
+scalaVersion := "2.13.2"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.1"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.1" % "test"
---
+++ src/test/scala/de/htwg/se/madn/model/PlaySpec.scala
@@ -0,0 +1,16 @@
+package de.htwg.se.madn.model
+import org.scalatest.{Matchers, WordSpec}
+
+class PlayerSpec extends WordSpec with Matchers {
+  "A Player" when { "new" should {
+    val player = Player("Your Name")
+    "have a name"  in {
+      player.name should be("Your Name")
+    }
+    "have a nice String representation" in {
+      player.toString should be("Your Name")
+    }
+  }}
+
+
+}
