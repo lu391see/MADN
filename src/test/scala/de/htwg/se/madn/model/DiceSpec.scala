e990448f1fbaee534a7882e587aaf1858f312880
Aron Lapp <Aron.Lapp@t-online.de>
2020-05-21 18:54:09 +0200

    updated DiceSpec

--- src/test/scala/de/htwg/se/madn/model/DiceSpec.scala
+++ src/test/scala/de/htwg/se/madn/model/DiceSpec.scala
@@ -2,25 +2,12 @@ package de.htwg.se.madn.model
 import org.scalatest.{Matchers, WordSpec}
 
 class DiceSpec extends WordSpec with Matchers{
-  "A Dice " when { "thrown" should {
-    val unthrownDice = Dice(1)
-    "have a value 0" in {
-      unthrownDice.roll should be(0)
-    }
-    "not be thrown" in {
-      unthrownDice.isThrown should be(false)
-    }
-  }
-  "set to a specific value" should {
-    val thrownDice = Dice(0)
-    "return that value" in {
-      thrownDice.value should be () //randint
-    }
-    "be thrown" in {
-      thrownDice.isThrown should be(true)
+  "A Dice " when {
+    "thrown" should {
+      val Dice = new Dice("yellow")
+      "have a value between 1 and 6" in {
+        Dice.t1 should (be > 0 and <7)
+      }
     }
   }
-
-  }
-
 }
