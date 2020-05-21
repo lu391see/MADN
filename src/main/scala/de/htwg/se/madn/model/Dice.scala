a17f3d8412ff1027841cd926976c1bda4d587fd2
Aron Lapp <Aron.Lapp@t-online.de>
2020-05-21 18:16:55 +0200

    changed 'range' to 'throw'

--- src/main/scala/de/htwg/se/madn/model/Dice.scala
+++ src/main/scala/de/htwg/se/madn/model/Dice.scala
@@ -2,6 +2,6 @@ package de.htwg.se.madn.model
 import scala.util.Random
 
 case class Dice(instantThrow:Int) {
-  val range: Int = Random.nextInt(5) + 1
-  println("You rolled %d", range)
+  val throw: Int = Random.nextInt(5) + 1
+  println("You rolled %d", throw)
 }
