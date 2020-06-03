76696189987c6fb74acad8734457f992a4560222
Aron Lapp <Aron.Lapp@t-online.de>
2020-06-03 17:08:57 +0200

    Added Player parameters Added Pins

--- src/main/scala/de/htwg/se/madn/model/Dice.scala
+++ src/main/scala/de/htwg/se/madn/model/Dice.scala
@@ -5,3 +5,18 @@ case class Dice(recipient : String) {
   val t1: Int = Random.nextInt(6) + 1
   val s1 = recipient + " has thrown " + t1.toString
 }
+
+def Init(Player:String){
+  Dice(Player)
+  if(Dice.t1 = 6) {
+    if(){ //Wenn Feld1 nicht belegt ist
+      //player.move....
+      //break
+    }
+
+  }
+}
+
+case class Move(recipient: String) {
+
+}
--- src/main/scala/de/htwg/se/madn/model/Player.scala
+++ src/main/scala/de/htwg/se/madn/model/Player.scala
@@ -1,5 +1,13 @@
 package de.htwg.se.madn.model
 
-case class Player(name: String) {
-  override def toString:String = name
+case class Player(name: String, override val index: Int, override val position: Int,
+                  override val team: Int, override val defaultPosition: Int,
+                  override val hasWon: Boolean = false) extends lPlayer {
+
+  def this(name: String) = this(name, player_counter, 0, player_counter, false)
+  val pins: List(0, 0, 0, 0) //Erstellt Liste mit Pins (Spielfiguren für Player)
+  pins.zipWithIndex //Outcome: List((0, 0), (0, 0), (0, 0), (0, 0)) der Form List((Position, Index), ...)
+
+
+  }
 }
