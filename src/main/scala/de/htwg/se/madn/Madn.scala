3148f6580f077cf3fa29aa6cb71a9edb8fa35bbe
Aron Lapp <Aron.Lapp@t-online.de>
2020-06-04 17:58:24 +0200

    made player size dynamic

--- src/main/scala/de/htwg/se/madn/Madn.scala
+++ src/main/scala/de/htwg/se/madn/Madn.scala
@@ -8,23 +8,20 @@ object Madn {
     val playerarraysize = scala.io.StdIn.readLine("Wieviele Spieler spielen?")
     var player_counter: Int = 1
     val players:Array[Player] = new Array[Player](playerarraysize.toInt)
-    players(player_counter - 1) = new Player(scala.io.StdIn.readLine("Type your Name: "), player_counter, (player_counter - 1) * 10)
-    val string1 = "Hello, " + players(0).name + ", Welcome to 'Mensch Aergere Dich Nicht'!"
+    var string1: String = "Hello, "
+    for (i <- 0 until  playerarraysize.toInt) {
+      players(player_counter - 1) = new Player(scala.io.StdIn.readLine("Type your Name: "), player_counter, (player_counter - 1) * 10)
+      string1 += players(player_counter-1).name
+      string1 += ", "
+      player_counter += 1
+    }
+    string1 += "Welcome to Mensch aergere dich nicht!"
+    player_counter -= 1
 
-
-    player_counter += 1
-
-    players(player_counter - 1) = new Player(scala.io.StdIn.readLine("2nd Player, type your Name: "), player_counter, (player_counter - 1)* 10)
-    val string2 = "Hello, " + players(1).name + ", Welcome to 'Mensch Aergere Dich Nicht'!"
-
-
-    var s = ""
-    s = "|" + string1 + "\n|" + string2 + "\n|" +
-      s"""The game will start with 2 player(s).
-         |Be careful $players(1).name + , you will face $players(0).name!\n"""
     var game = new Field[Cell](40, Cell(0))
     val tui = new Tui
-    println(s.stripMargin)
+    //println(s.stripMargin)
+    println(string1)
 
     var turn_counter = 0
 
@@ -34,7 +31,7 @@ object Madn {
     while(input != "q") {
 
       val dice = Dice()
-      println(players(turn_counter).name + " can walk " + dice.t1)
+      println(players(turn_counter).name + " can walk " + dice.t1 + " please choose a pin to walk with (1-4)!")
       input = scala.io.StdIn.readLine()
       game = tui.processInputLine(input, game, players(turn_counter), dice)
 
