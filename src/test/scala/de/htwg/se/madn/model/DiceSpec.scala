24996e9746f1ab4949d12fba8b2724e1d9ed1127
lu391see <lukas.seehuber@gmail.com>
2020-06-04 19:16:58 +0200

    simplified dice test

--- src/test/scala/de/htwg/se/madn/model/DiceSpec.scala
+++ src/test/scala/de/htwg/se/madn/model/DiceSpec.scala
@@ -4,16 +4,10 @@ import org.scalatest.{Matchers, WordSpec}
 class DiceSpec extends WordSpec with Matchers{
   "A Dice " when {
     "thrown" should {
-      val dice = new Dice("yellow")
+      val dice = new Dice()
       "have a value between 1 and 6" in {
         dice.t1.isInstanceOf[Int]
         dice.t1.toString matches "1|2|3|4|5|6"
-      }
-      "when printed look like" in {
-        dice.s1 matches s"$dice.recipient has thrown $dice.t1"
-      }
-      "recipient as Team" in {
-        dice.recipient should be("yellow")
       }
     }
   }
