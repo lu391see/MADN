2d7f28ab715dcc9f529436bea26b1375b584b615
Aron Lapp <Aron.Lapp@t-online.de>
2020-06-11 23:32:42 +0200

    Merge branch 'Controller' of C:\Users\Bob\Desktop\MADN with conflicts.

--- src/main/scala/de/htwg/se/madn/model/Player.scala
+++ src/main/scala/de/htwg/se/madn/model/Player.scala
@@ -1,18 +1,30 @@
 package de.htwg.se.madn.model
 
-case class Player(name: String,
-                  team: Int, defaultPosition: Int) {
-  var hasWon :Boolean = false
+
+case class Player(name: String, team: Int) {
   //def this(name: String) = this(name, team, defaultPosition, false)
-  val pin1 = new Pin(team * 10 + 1)
+  val defaultPosition : Int = (team-1) * 10
+
+  var hasWon :Boolean = false
+  val pins:Array[Pin] = new Array[Pin](4)
+  for(i <- 0 until 5) {
+    pins(i) = new Pin(team * 10 + 1)
+    pins(i).addPosition(defaultPosition)
+  }
+  /*
+  val pin1 = Pin(team * 10 + 1)
   pin1.addPosition(defaultPosition)
 
-  val pin2 = new Pin(team * 10 + 2)
+  val pin2 = Pin(team * 10 + 2)
   pin2.addPosition(defaultPosition)
 
-  val pin3 = new Pin(team * 10 + 3)
+  val pin3 = Pin(team * 10 + 3)
   pin3.addPosition(defaultPosition)
 
-  val pin4 = new Pin(team * 10 + 4)
+  val pin4 = Pin(team * 10 + 4)
   pin4.addPosition(defaultPosition)
+  */
+  override def toString: String = {
+    "Player " + team + ": " + name
+  }
 }
