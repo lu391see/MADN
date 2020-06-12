710ac8d9df811b1670e04d0c335ac016275a438b
Aron Lapp <Aron.Lapp@t-online.de>
2020-06-12 09:45:45 +0200

    remove lPlayer

--- src/main/scala/de/htwg/se/madn/controller/Controller.scala
+++ src/main/scala/de/htwg/se/madn/controller/Controller.scala
@@ -1,8 +1,22 @@
 package de.htwg.se.madn.controller
 
-import de.htwg.se.madn.model.{Cell, Field}
+import de.htwg.se.madn.model.{Cell, Field, Player, Dice}
 import de.htwg.se.madn.util.Observable
 
-class Controller(var field: Field[Cell]) extends Observable{
-
+class Controller(var board: Field[Cell]) extends Observable{
+  def move(player: Player, input: String, dice: Dice): Unit = {
+    //var board : Field[Cell] = game
+    if (player.pins(input.toInt - 1).position + dice.t1 < player.defaultPosition + 40) {
+      board = board.replaceCell(player.pins(input.toInt - 1).position, Cell(0))
+      player.pins(input.toInt - 1).addPosition(dice.t1)
+      board = board.replaceCell(player.pins(input.toInt-1).position, Cell(player.pins(input.toInt - 1).index))
+      //println(player.pins(input.toInt - 1).position + dice.t1)
+    }
+    else {
+      player.hasWon = true
+      board.replaceCell(player.pins(input.toInt - 1).position, Cell(0))
+    }
+    board
+    notifyObservers
+  }
 }
--- src/main/scala/de/htwg/se/madn/model/lPlayer.scala
+++
@@ -1,9 +0,0 @@
-package de.htwg.se.madn.model
-
-trait lPlayer{
-  val index : Int
-  val position : Int
-  val team : Int
-  val defaultPosition : Int
-  val hasWon: Boolean
-}
