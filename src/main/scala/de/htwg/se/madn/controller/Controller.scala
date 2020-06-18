cb6de97de89deee1353dcff3d57ff92ff5c79da7
Aron Lapp <Aron.Lapp@t-online.de>
2020-06-18 21:25:15 +0200

    remade throwing function to replace the former occupant to his default position. Removed moved pins old positions from Field

--- src/main/scala/de/htwg/se/madn/controller/Controller.scala
+++ src/main/scala/de/htwg/se/madn/controller/Controller.scala
@@ -4,26 +4,54 @@ import de.htwg.se.madn.model.{Cell, Dice, Field, Pin, Player}
 import de.htwg.se.madn.util.Observable
 
 class Controller(var board: Field[Cell]) extends Observable{
-  def move(player: Player, input: String, dice: Dice): Unit = {
-    //var board : Field[Cell] = game
-    if (player.pins(input.toInt - 1).position + dice.t1 < player.defaultPosition + 40) {
-      board = board.replaceCell(player.pins(input.toInt - 1).position, Cell(0))
-      player.pins(input.toInt - 1).addPosition(dice.t1)
-    }
-      //board = board.replaceCell(player.pins(input.toInt-1).position, Cell(player.pins(input.toInt - 1).index))
-    if (board.cell(player.pins(input.toInt - 1).position + dice.t1) != Cell(0)) {
-      Pin(board.cell(player.pins(input.toInt - 1).position + dice.t1).value).position = 0
-      board = board.replaceCell(player.pins(input.toInt - 1).position + dice.t1, Cell(player.pins(input.toInt - 1).index))
-      player.pins(input.toInt - 1).addPosition(dice.t1)
+  def move(player: Player, input: String, dice: Dice, playerlist: List[Player]): Unit = {
+    if(player.pins(input.toInt - 1).position + dice.t1 > player.defaultPosition + 40) {
+      if (board.cell(player.pins(input.toInt - 1).position + dice.t1 - 40) != Cell(0)) {
+        //isMyTeam: Drawing pin / pin at position
+        val isMyTeam : Int = Pin(board.cell(player.pins(input.toInt - 1).position + dice.t1).value).index / player.pins(input.toInt - 1).index
+        if(isMyTeam != 0) {
+          //set old occupants position to default
+          val calcTeam = board.cell(player.pins(input.toInt - 1).position + dice.t1).value / 10
+          Pin(board.cell(player.pins(input.toInt - 1).position + dice.t1).value).addPosition(-(player.pins(input.toInt - 1).position + dice.t1))
+          Pin(board.cell(player.pins(input.toInt - 1).position + dice.t1).value).addPosition(playerlist(calcTeam).defaultPosition)
+
+
+          Pin(board.cell(player.pins(input.toInt - 1).position + dice.t1).value).position = 0
+          board = board.replaceCell(player.pins(input.toInt - 1).position, Cell(0))
+          board = board.replaceCell(player.pins(input.toInt - 1).position + dice.t1 - 40, Cell(player.pins(input.toInt - 1).index))
+          player.pins(input.toInt - 1).addPosition(dice.t1)
+          player.pins(input.toInt - 1).addPosition(-40)
+        }
+      }
+      else {
+        board = board.replaceCell(player.pins(input.toInt - 1).position + dice.t1 - 40, Cell(player.pins(input.toInt - 1).index))
+        player.pins(input.toInt - 1).addPosition(dice.t1)
+        player.pins(input.toInt - 1).addPosition(-40)
+      }
     }
     else {
-      player.pins(input.toInt - 1).addPosition(dice.t1)
-      board = board.replaceCell(player.pins(input.toInt-1).position, Cell(player.pins(input.toInt - 1).index))
+      if (board.cell(player.pins(input.toInt - 1).position + dice.t1) != Cell(0)) {
+        //isMyTeam: Drawing pin / pin at position
+        val isMyTeam : Int = Pin(board.cell(player.pins(input.toInt - 1).position + dice.t1).value).index / player.pins(input.toInt - 1).index
+        if (isMyTeam != 0) {
+          //set old occupants position to default
+          val calcTeam = board.cell(player.pins(input.toInt - 1).position + dice.t1).value / 10
+          Pin(board.cell(player.pins(input.toInt - 1).position + dice.t1).value).addPosition(-(player.pins(input.toInt - 1).position + dice.t1))
+          Pin(board.cell(player.pins(input.toInt - 1).position + dice.t1).value).addPosition(playerlist(calcTeam).defaultPosition)
+
+          board = board.replaceCell(player.pins(input.toInt - 1).position, Cell(0))
+          board = board.replaceCell(player.pins(input.toInt - 1).position + dice.t1, Cell(player.pins(input.toInt - 1).index))
+          player.pins(input.toInt - 1).addPosition(dice.t1)
+          }
+        }
+      else {
+        board = board.replaceCell(player.pins(input.toInt - 1).position, Cell(0))
+        board = board.replaceCell(player.pins(input.toInt - 1).position + dice.t1, Cell(player.pins(input.toInt - 1).index))
+        player.pins(input.toInt - 1).addPosition(dice.t1)
+      }
     }
-    /*else {
-      player.hasWon = true
-      board.replaceCell(player.pins(input.toInt - 1).position, Cell(0))
-    }*/
+
+
     board
     notifyObservers
   }
